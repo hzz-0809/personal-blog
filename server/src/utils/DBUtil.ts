26197010aa647fa8f8ed98010f2edf337f1a0021
hzz-0809 <70731747+hzz-0809@users.noreply.github.com>
2021-03-02 11:35:48 +0800

    Update DBUtil.ts

--- server/src/utils/DBUtil.ts
+++ server/src/utils/DBUtil.ts
@@ -6,8 +6,7 @@ class DBUtil {
             // host: '115.29.177.15',
             host:'127.0.0.1',
             user: 'root',
-            password: '187765',
-            // password:'Huang..187765',
+            password: '123456',//mysql密码
             database: 'blog',
             charset: 'utf8mb4'
         });
